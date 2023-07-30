9ff3d7d8145b48c24d93d583158fa1b4a68d53ea
Saikat Sheet <SaikatSheet48@gmail.com>
2023-07-30 08:32:31 +0530

    adding solution

---
+++ GeeksforGeeks/Inorder Successor in BST.py
@@ -0,0 +1,139 @@
+# Given a BST, and a reference to a Node x in the BST. Find the Inorder Successor of the given node in the BST.
+
+# Example 1:
+
+# Input:
+#      2
+#    /   \
+#   1     3
+# K(data of x) = 2
+# Output: 3 
+# Explanation: 
+# Inorder traversal : 1 2 3 
+# Hence, inorder successor of 2 is 3.
+
+# Example 2:
+
+# Input:
+#             20
+#            /   \
+#           8     22
+#          / \
+#         4   12
+#            /  \
+#           10   14
+# K(data of x) = 8
+# Output: 10
+# Explanation:
+# Inorder traversal: 4 8 10 12 14 20 22
+# Hence, successor of 8 is 10.
+
+
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self, val, k):
+        self.right = None
+        self.data = val
+        self.left = None
+        self.key = k
+'''
+class Solution:
+    # returns the inorder successor of the Node x in BST (rooted at 'root')
+    def inorderSuccessor(self, root, x):
+        # Code here
+        succ = Node(-1)
+        
+        while root != None:
+            if root.data>x.data:
+                succ = root
+                root = root.left
+                
+            else:
+                root = root.right
+                
+        return succ
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        k = int(input())
+        ptr = Solution().inorderSuccessor(root, Node(k))
+        if ptr is None:
+            print(-1)
+        else:
+            print(ptr.data)
+# } Driver Code Ends
