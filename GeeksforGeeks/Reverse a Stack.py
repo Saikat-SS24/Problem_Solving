db95c6e4f2b70b1935520887f9b55a68657c996c
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-04 16:19:52 +0530

    adding solution

---
+++ GeeksforGeeks/Reverse a Stack.py
@@ -0,0 +1,48 @@
+# You are given a stack St. You have to reverse the stack using recursion.
+
+# Example 1:
+
+# Input:
+# St = {3,2,1,7,6}
+# Output:
+# {6,7,1,2,3}
+# Explanation:
+# Input stack after reversing will look like the stack in the output.
+# Example 2:
+
+# Input:
+# St = {4,3,9,6}
+# Output:
+# {6,9,3,4}
+# Explanation:
+#Input stack after reversing will look like the stack in the output.
+
+#User function Template for python3
+
+from typing import List
+
+class Solution:
+    def reverse(self,St): 
+        #code here
+        list=[]
+        for i in range(len(St)):
+            list.append(St.pop())
+            
+        for j in list:
+            St.append(j)
+        return St
+
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+ 
+for _ in range(int(input())):
+    N = int(input())
+    St = list(map(int, input().split()))
+    ob = Solution()
+    ob.reverse(St)
+    print(*St)
+# } Driver Code Ends
