38204a0bd5301cf80360b2e165a8741cbd3913cd
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-06 16:38:38 +0530

    adding solution

---
+++ GeeksforGeeks/String Permutations.py
@@ -0,0 +1,45 @@
+""" Given a string S. The task is to find all permutations (need not be different) of a given string.
+
+Example 1:
+
+Input:
+S = AAA
+Output: AAA AAA AAA AAA AAA AAA
+Explanation: There are total 6 permutations, as given in the output.
+Example 2:
+
+Input:
+S = ABSG
+Output: ABGS ABSG AGBS AGSB ASBG ASGB
+BAGS BASG BGAS BGSA BSAG BSGA GABS
+GASB GBAS GBSA GSAB GSBA SABG SAGB
+SBAG SBGA SGAB SGBA
+Explanation: There are total 24 permutations, as given in the output. """
+
+
+#User function Template for python3
+from itertools import permutations
+    
+class Solution:
+    def permutation(self,s):
+        permu = permutations(s)
+        sorted_permutations = sorted([''.join(p) for p in permu])
+        return sorted_permutations
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    T=int(input())
+    while(T>0):
+        s=input()
+        ob=Solution()
+        ans=ob.permutation(s)
+        for i in ans:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+# } Driver Code Ends
