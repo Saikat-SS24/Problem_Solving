067ada8fc0238f08b3f8b51ea32dbe79c89f3cfa
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-07 22:27:37 +0530

    updating comment

--- GeeksforGeeks/Solve the Sudoku.py
+++ GeeksforGeeks/Solve the Sudoku.py
@@ -11,15 +11,16 @@ the element in the cell which is not blank.
 
 
 Sample Sudoku for you to get the logic for its solution:
-3   6  5  8  4  
-5 2 
-  8 7         3  1
-    3    1    8
-9     8  6 3     5
-  5      9   6
-1 3          2 5
-               7 4
-    5 2    6 3
+
+|3| |6|5| |8|4| | |
+|5|2| | | | | | | |
+| |8|7| | | | |3|1|
+| | |3| |1| | |8| |
+|9| | |8|6|3| | |5|
+| |5| | |9| |6| | |
+|1|3| | | | |2|5| |
+| | | | | | | |7|4|
+| | |5|2| |6|3| | |
 
 
 Example 1:
