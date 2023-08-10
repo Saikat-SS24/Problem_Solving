641fa3616d6b8402c6141cb4c2146501e683bfe5
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-10 11:28:05 +0530

    adding solution

---
+++ GeeksforGeeks/Longest Common Subsequence.py
@@ -0,0 +1,57 @@
+""" Given two strings, find the length of longest subsequence present in both of them. Both the strings are in uppercase latin alphabets.
+
+Example 1:
+
+Input:
+A = 6, B = 6
+str1 = ABCDGH
+str2 = AEDFHR
+Output: 3
+Explanation: LCS for input strings “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+Example 2:
+
+Input:
+A = 3, B = 2
+str1 = ABC
+str2 = AC
+Output: 2
+Explanation: LCS of "ABC" and "AC" is "AC" of length 2. """
+
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the length of longest common subsequence in two strings.
+    def lcs(self,x,y,s1,s2):
+        
+        # code here
+        matrix = [[0]*(y+1) for q in range(x+1)]
+        for i in range(1,x+1):
+            for j in range(1,y+1):
+                if s1[i-1] == s2[j-1]:
+                    matrix[i][j] = matrix[i-1][j-1]+1
+                else:
+                    matrix[i][j] = max(matrix[i-1][j],matrix[i][j-1])
+                    
+        return matrix[x][y]
+                    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+# Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        x,y = map(int,input().strip().split())
+        s1 = str(input())
+        s2 = str(input())
+        ob=Solution()
+        print(ob.lcs(x,y,s1,s2))
+# } Driver Code Ends
