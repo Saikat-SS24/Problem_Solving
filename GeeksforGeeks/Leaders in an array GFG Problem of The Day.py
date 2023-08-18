701495dbadbf3857280b2d6396eb079e4e3399c0
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-18 20:19:41 +0530

    adding solutions

---
+++ GeeksforGeeks/Leaders in an array GFG Problem of The Day.py
@@ -0,0 +1,80 @@
+""" Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
+
+Example 1:
+
+Input:
+n = 6
+A[] = {16,17,4,3,5,2}
+Output: 17 5 2
+Explanation: The first leader is 17 
+as it is greater than all the elements
+to its right.  Similarly, the next 
+leader is 5. The right most element 
+is always a leader so it is also 
+included.
+Example 2:
+
+Input:
+n = 5
+A[] = {1,2,3,4,0}
+Output: 4 0
+Explanation: 0 is the rightmost element
+and 4 is the only element which is greater
+than all the elements to its right.
+Your Task:
+You don't need to read input or print anything. The task is to complete the function leader() which takes array A and n as input parameters and returns an array of leaders in order of their appearance.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1 <= n <= 107
+0 <= Ai <= 107 """
+
+class Solution:
+    #Back-end complete function Template for Python 3
+    
+    #Function to find the leaders in the array.
+    def leaders(self, A, N):
+        #Code here
+        value=0
+        leaders=[]
+        for i in reversed(range(N)):
+            if(value<=A[i]):
+                value=A[i]
+                leaders.append(value)
+        leaders.reverse()
+        return leaders
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        N=int(input())
+        
+        A=[int(x) for x in input().strip().split()]
+        obj = Solution()
+        
+        A=obj.leaders(A,N)
+        
+        for i in A:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
