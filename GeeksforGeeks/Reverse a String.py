d5682325d3465b2205ec661333b34cfbb9faf23e
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-27 11:26:24 +0530

    adding solution

---
+++ GeeksforGeeks/Reverse a String.py
@@ -0,0 +1,42 @@
+""" You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+Your Task:
+
+You only need to complete the function reverseWord() that takes s as parameter and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000 """
+
+
+#User function Template for python3
+
+class Solution:
+    def reverseWord(self,s):
+        #your code here
+        return s[::-1]
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while(t>0):
+        s = input()
+        ob = Solution()
+        print(ob.reverseWord(s))
+        t = t-1
+
+# } Driver Code Ends
