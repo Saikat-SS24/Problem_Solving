53b0ee012ad55f05312c6e8e45ab83fe894d8eb7
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-29 10:42:53 +0530

    adding solution

---
+++ GeeksforGeeks/Delete nodes having greater value on right.py
@@ -0,0 +1,140 @@
+""" Given a singly linked list, remove all the nodes in the list which have any node on their right whose value 
+is greater. (Not just immidiate Right , but entire List on the Right)
+
+Example 1:
+
+Input:
+LinkedList = 12->15->10->11->5->6->2->3
+Output: 15 11 6 3
+Explanation: Since, 12, 10, 5 and 2 are
+the elements which have greater elements
+on the following nodes. So, after deleting
+them, the linked list would like be 15,
+11, 6, 3.
+Example 2:
+
+Input:
+LinkedList = 10->20->30->40->50->60
+Output: 60
+Explanation: All the nodes except the last
+node has a greater value node on its right,
+so all the nodes except the last node must
+be removed.
+Your Task:
+The task is to complete the function compute() which should modify the list as required and return the head of the modified linked list. The printing is done by the driver code,
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ size of linked list ≤ 105
+1 ≤ element of linked list ≤ 105 """
+
+
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self,x):
+        self.data=x
+        self.next=None
+
+'''
+class Solution:
+    def compute(self,head):
+        #Your code here
+        def reverse(head):
+            prev=None
+            curr=head
+            while curr:
+                temp=curr.next
+                curr.next=prev
+                prev=curr
+                curr=temp
+            return prev
+        new=reverse(head)
+        max=new.data
+        prev=new
+        curr=new.next
+        while curr:
+            if curr.data>=max:
+                prev.next=curr
+                prev=curr
+                max=curr.data
+            curr=curr.next
+        prev.next=None
+        return reverse(new)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail =new_node
+            return
+        self.tail.next=new_node
+        self.tail=new_node
+
+    def getNode(self,value): # return node with given value, if not present return None
+        curr_node=self.head
+        while(curr_node.next and curr_node.data != value):
+            curr_node=curr_node.next
+        if(curr_node.data==value):
+            return curr_node
+        else:
+            return None
+
+    # prints the elements of linked list starting with head
+    def printList(self):
+        if self.head is None:
+            print(' ')
+            return
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data,end=" ")
+            curr_node=curr_node.next
+        print(' ')
+
+
+
+
+
+if __name__=="__main__":
+    t=int(input())
+    while(t>0):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        
+        
+        result=Solution().compute(a.head)
+        a.head=result
+        a.printList()
+        t-=1
+    
+        
+    
+    
+# } Driver Code Ends
