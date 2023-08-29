26dc444872ed75be3f3237106fcadffd3c2c6fac
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-29 11:17:24 +0530

    adding solution

---
+++ GeeksforGeeks/Check if two arrays are equal or not.py
@@ -0,0 +1,74 @@
+""" Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are 
+said to be equal if both of them contain same set of elements, arrangements (or permutation) of elements may be 
+different though.
+Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,5,4,0}
+B[] = {2,4,5,0,1}
+Output: 1
+Explanation: Both the array can be 
+rearranged to {0,1,2,4,5}
+Example 2:
+
+Input:
+N = 3
+A[] = {1,2,5}
+B[] = {2,4,15}
+Output: 0
+Explanation: A[] and B[] have only 
+one common value.
+Your Task:
+Complete check() function which takes both the given array and their size as function arguments and returns true if the arrays are equal else returns false.The 0 and 1 printing is done by the driver code.
+
+Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(N)
+
+Constraints:
+1<=N<=107
+1<=A[],B[]<=1018 """
+
+#User function Template for python3
+
+class Solution:
+    #Function to check if two arrays are equal or not.
+    def check(self,A,B,N):
+        
+        #return: True or False
+        
+        #code here
+        if len(A) != len(B):
+            return False
+        A.sort()
+        B.sort()
+        for i in range(0,N):
+            if(A[i] != B[i]):
+                return False
+        
+        return True  # if all elements are same 
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for tc in range(t):
+        
+        N=int(input())
+        
+        A = [int(x) for x in input().replace('  ',' ').strip().split(' ')]
+        B = [int(x) for x in input().replace('  ',' ').strip().split(' ')]
+        ob=Solution()
+        if ob.check(A,B,N):
+            print(1)
+        else:
+            print(0)
+        
+                
+                
+# } Driver Code Ends
