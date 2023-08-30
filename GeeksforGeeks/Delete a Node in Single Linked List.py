4e9a113e8d829c0dbaf76561eaf81998cf862ef1
Saikat Sheet <SaikatSheet48@gmail.com>
2023-08-30 10:46:23 +0530

    adding solution

---
+++ GeeksforGeeks/Delete a Node in Single Linked List.py
@@ -0,0 +1,105 @@
+""" Given a singly linked list and an integer x.Delete xth node from the singly linked list.
+
+Example 1:
+
+Input: 1 -> 3 -> 4 
+       x = 3
+Output: 1 -> 3
+Explanation:
+After deleting the node at 3rd
+position (1-base indexing), the
+linked list is as 1 -> 3. 
+Example 2:
+
+Input: 1 -> 5 -> 2 -> 9 
+x = 2
+Output: 1 -> 2 -> 9
+Explanation: 
+After deleting the node at 2nd
+position (1-based indexing), the
+linked list is as 1 -> 2 -> 9.
+Your task: Your task is to complete the method deleteNode() which takes two arguments: the address of the head of the linked list and an integer x. The function returns the head of the modified linked list.
+
+Constraints:
+2 <= N <= 105
+1 <= x <= N """
+
+# your task is to complete this function
+# function should return new head pointer
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+def delNode(head, k):
+    # Code here
+    temp=head
+    prev=head
+    for i in range(0,k):
+        if i==0 and k==1:
+            head=head.next
+        else:
+            if i==k-1 and temp is not None:
+                prev.next=temp.next
+            else:
+                prev=temp
+                # position was greater than number of nodes in the list
+                if prev is None:
+                    break
+                temp=temp.next
+    return head
+
+#{ 
+ # Driver Code Starts
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.last = None
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.head.data = data
+            self.last = self.head
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            self.last.next = new_node
+            self.last = new_node
+            # self.
+            # temp = self.head
+            # while(temp.next):
+            #     temp=temp.next
+            # temp.next = new_node
+
+def printlist(head):
+    temp = head
+    while(temp):
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        k = int(input())
+        newhead = delNode(list1.head, k)
+        printlist(newhead)
+# Contributed By: Harshit Sidhwa
+# } Driver Code Ends
