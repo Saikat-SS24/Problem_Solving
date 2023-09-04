cc63366e029b26752234f90a046bf8c8ed1cb752
Saikat Sheet <SaikatSheet48@gmail.com>
2023-09-04 20:01:36 +0530

    adding solution

---
+++ LeetCode/Linked List Cycle.py
@@ -0,0 +1,57 @@
+""" Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously 
+following the next pointer. Internally, pos is used to denote the index of the node that tail's 
+next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+ 
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list. """
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # create a temporary node
+        temp=""
+        while(head != None):
+            # this condition is for the case when there is no loop
+            if(head.next == None):
+                return False
+            # check if next is already pointing to temp
+            if(head.next == temp):
+                return True
+            # store the pointer to the next node in order to get it in the next step
+            next=head.next
+            # make next point to temp
+            head.next=temp
+            # get to the next node in the list
+            head=next
+            
+        return False
+        
