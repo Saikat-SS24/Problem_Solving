bb76c674edf2ddd851dbe46edd9fdc32e774f969
Saikat Sheet <saikatsheet48@gmail.com>
2023-10-09 16:57:09 +0530

    adding solution

---
+++ LeetCode/Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,35 @@
+""" Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given 
+target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+ 
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109 """
+
+
+class Solution:
+  def searchRange(self, nums: List[int], target: int) -> List[int]:
+    l = bisect_left(nums, target)
+    if l == len(nums) or nums[l] != target:
+      return -1, -1
+    r = bisect_right(nums, target) - 1
+    return l, r
