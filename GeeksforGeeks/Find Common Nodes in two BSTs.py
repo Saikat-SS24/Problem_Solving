12955dd0d48ecdd7013253f58b90465738e3abbb
Saikat Sheet <saikatsheet48@gmail.com>
2023-10-14 10:47:10 +0530

    uploading solution

---
+++ GeeksforGeeks/Find Common Nodes in two BSTs.py
@@ -0,0 +1,152 @@
+""" Given two Binary Search Trees. Find the nodes that are common in both of them, ie- find the intersection of the 
+two BSTs.
+
+Note: Return the common nodes in sorted order.
+
+Example 1:
+
+Input:
+BST1:
+                  5
+               /     \
+             1        10
+           /   \      /
+          0     4    7
+                      \
+                       9
+BST2:
+                10 
+              /    \
+             7     20
+           /   \ 
+          4     9
+Output: 4 7 9 10
+
+Example 2:
+
+Input:
+BST1:
+     10
+    /  \
+   2   11
+  /  \
+ 1   3
+BST2:
+       2
+     /  \
+    1    3
+Output: 1 2 3
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findCommon() that takes roots of
+the two BSTs as input parameters and returns a list of integers containing the common nodes in sorted order. 
+
+Expected Time Complexity: O(N1 + N2) where N1 and N2 are the sizes of the 2 BSTs.
+Expected Auxiliary Space: O(H1 + H2) where H1 and H2 are the heights of the 2 BSTs.
+
+Constraints:
+1 <= Number of Nodes <= 105
+1 <= Node data <= 109 """
+
+
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to find the nodes that are common in both BST.
+    def findCommon(self, root1, root2):
+        # code here
+        def traverse(root):
+            if root.left: traverse(root.left)
+            li.append(root.data)
+            if root.right: traverse(root.right)
+        li = []
+        traverse(root1)
+        l = li.copy()
+        li.clear()
+        traverse(root2)
+        return sorted(list(set(l)&set(li)))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root1=buildTree(s)
+        s=input()
+        root2=buildTree(s)
+        ob = Solution()
+        res = ob.findCommon(root1, root2)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print()
+        
+# } Driver Code Ends
