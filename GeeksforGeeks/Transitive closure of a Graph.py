2562c222f450669d83dc9977076ee6f38cdbf4d8
Saikat Sheet <saikatsheet48@gmail.com>
2023-10-17 10:37:01 +0530

    uploading solution

---
+++ GeeksforGeeks/Transitive closure of a Graph.py
@@ -0,0 +1,72 @@
+""" Given a directed graph, determine whether a vertex j is reachable from another vertex i for all vertex pairs 
+(i, j) in the given graph. Here, vertex j is reachable from another vertex i means that there is a path from vertex i 
+to j. The reachability matrix is called the transitive closure of a graph. The directed graph is represented by an 
+adjacency matrix where there are N vertices. 
+
+Example 1:
+
+Input: N = 4
+graph = {{1, 1, 0, 1}, 
+         {0, 1, 1, 0}, 
+         {0, 0, 1, 1}, 
+         {0, 0, 0, 1}}
+Output: {{1, 1, 1, 1}, 
+         {0, 1, 1, 1}, 
+         {0, 0, 1, 1}, 
+         {0, 0, 0, 1}}
+Explanation: 
+The output list shows the reachable indexes.
+Example 2:
+
+Input: N = 3
+graph = {{1, 0, 0}, 
+         {0, 1, 0}, 
+         {0, 0, 1}
+Output: {{1, 0, 0}, 
+         {0, 1, 0}, 
+         {0, 0, 1}
+Explanation: 
+The output list shows the reachable indexes.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function transitiveClosure() which takes
+an integer N and a 2D array graph(adjacency matrix of the graph) as input parameters and returns the transitive closure
+of the graph in the form of 2D array.
+
+Expected Time Complexity: O(N3)
+Expected Auxiliary Space: O(N2)
+
+Constraints:
+1 ≤ N ≤ 100  """
+
+
+#User function Template for python3
+
+class Solution:
+    def transitiveClosure(self, N, graph):
+        # code here
+        for k in range(N):
+            for i in range(N):
+                for j in range(N):
+                    if (graph[i][k] and graph[k][j]) or i == j:
+                        graph[i][j] = 1
+        return graph
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        graph = []
+        for i in range(0,N):
+            graph.append([int(j) for j in input().split()])
+        ob = Solution()
+        ans = ob.transitiveClosure(N, graph)
+        for i in range(N):
+            for j in range(N):
+                print(ans[i][j], end = " ")
+            print()
+# } Driver Code Ends
