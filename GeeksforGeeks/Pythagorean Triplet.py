7379b07e33a27dc694a1fcc4d25e87da7ef20e0b
Saikat Sheet <saikatsheet48@gmail.com>
2023-11-03 11:31:13 +0530

    uploading solution

---
+++ GeeksforGeeks/Pythagorean Triplet.py
@@ -0,0 +1,68 @@
+""" Given an array arr of n integers, write a function that returns true if there is a triplet (a, b, c) from the array
+(where a, b, and c are on different indexes) that satisfies a2 + b2 = c2, otherwise return false.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {3, 2, 4, 6, 5}
+Output: Yes
+Explanation: a=3, b=4, and c=5 forms a
+pythagorean triplet.
+
+Example 2:
+
+Input:
+N = 3
+Arr[] = {3, 8, 5}
+Output: No
+Explanation: No such triplet possible.
+Your Task:
+You don't have to take any input or print any thing. You have to complete the function checkTriplet() which takes an 
+array arr, a single integer n, as input parameters and returns boolean denoting answer to the problem.
+Note: The driver will print "Yes" or "No" instead of corresponding to the boolean value returned.
+
+Expected Time Complexity: O(n+max(Arr[i])2)
+Expected Auxiliary Space: O(max(Arr[i]))
+
+Constraints:
+1 <= n <= 105
+1 <= arr[i] <= 1000 """
+
+
+#User function Template for python3
+class Solution:
+
+	def checkTriplet(self,arr, n):
+    	# code here
+    	res = {}
+        for i in arr:
+            res[i**2]=1
+        for i in range(n):
+            for j in range(i+1,n):
+                if(arr[i]**2 + arr[j]**2 in res):
+                    return True
+        return False
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.checkTriplet(arr, n)
+        if ans:
+            print("Yes")
+        else:
+            print("No")
+        tc -= 1
+
+
+# } Driver Code Ends
