c4bb198b60fd4042ff8b512e6020f272d5569ece
Saikat Sheet <saikatsheet48@gmail.com>
2024-03-16 11:36:14 +0530

    uploading solutions

---
+++ GeeksforGeeks/Delete without head pointer.py
@@ -0,0 +1,146 @@
+""" You are given a node del_node of a Singly Linked List where you have to delete a value of the given node from the 
+linked list but you are not given the head of the list.
+
+By deleting the node value, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before & after the del_node node should be in the same order.
+Note:
+
+Multiple nodes can have the same values as the del_node, but you must only remove the node whose pointer del_node is 
+given to you.
+It is guaranteed that there exists a node with a value equal to the del_node value and it will not be the last node of 
+the linked list.
+
+Example 1:
+
+Input:
+Linked List = 1 -> 2
+del_node = 1
+Output: 
+2
+Explanation: 
+After deleting 1 from the linked list, 
+we have remaining nodes as 2.
+
+Example 2:
+
+Input:
+Linked List = 10 -> 20 -> 4 -> 30
+del_node = 20
+Output: 
+10 4 30
+Explanation: 
+After deleting 20 from the linked list, 
+we have remaining nodes as 10, 4, 30.
+Your Task:
+You don't need to read or print anything. You only need to complete the function deleteNode() which takes a reference 
+of the deleting node value & your task is to delete the given node value.
+
+Expected Time Complexity: O(1).
+Expected Auxilliary Space: O(1).
+
+Constraints:
+2 <= n <= 103  
+1 <= elements of the linked list <= 109 """
+
+#User function Template for python3
+'''
+    Your task is to delete the given node from
+	the linked list, without using head pointer.
+	
+	Function Arguments: node (given node to be deleted) 
+	Return Type: None, just delete the given node from the linked list.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
+class Solution:
+    #Function to delete a node without any reference to head pointer.
+    def deleteNode(self,del_node):
+        #code here
+        del_node.data=del_node.next.data
+        del_node.next=del_node.next.next
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+#Contributed by : Nagendra Jha
+
+import atexit
+import io
+import sys
+sys.setrecursionlimit(5000)
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail =new_node
+            return
+        self.tail.next=new_node
+        self.tail=new_node
+
+    def getNode(self,value): # return node with given value, if not present return None
+        curr_node=self.head
+        while(curr_node.next and curr_node.data != value):
+            curr_node=curr_node.next
+        if(curr_node.data==value):
+            return curr_node
+        else:
+            return None
+
+    # prints the elements of linked list starting with head
+    def printList(self):
+        if self.head is None:
+            print(' ')
+            return
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data,end=" ")
+            curr_node=curr_node.next
+        print()
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList() # create a new linked list 'a'.
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        del_elem = int(input())
+        del_node=a.getNode(del_elem)
+        Solution().deleteNode(del_node)
+        a.printList()
+
+# } Driver Code Ends
