bd85282a9cc4ded5ecb85f8b0921fe9881aa23c4
Saikat Sheet <saikatsheet48@gmail.com>
2024-03-22 12:14:29 +0530

    uploading solutions

---
+++ LeetCode/Perfect Number.py
@@ -0,0 +1,38 @@
+""" A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number 
+itself. A divisor of an integer x is an integer that can divide x evenly.
+
+Given an integer n, return true if n is a perfect number, otherwise return false.
+
+Example 1:
+
+Input: num = 28
+Output: true
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+1, 2, 4, 7, and 14 are all divisors of 28.
+
+Example 2:
+
+Input: num = 7
+Output: false
+ 
+Constraints:
+
+1 <= num <= 108 """
+
+
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+        sum = 1
+        left = 2
+        right = num / 2
+        while (left < right):
+            if (num % left == 0): 
+                sum += left
+                sum += num / left
+                right = num / left
+
+            left += 1
+
+        return sum == num;      
