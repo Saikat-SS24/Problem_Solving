8cf089c2b050f674cb4a87c445e40f44cfefde8f
Saikat Sheet <saikatsheet48@gmail.com>
2024-07-14 11:47:52 +0530

    uploading solution

---
+++ GeeksforGeeks/Segregate 0s and 1s.py
@@ -0,0 +1,56 @@
+""" Given an array arr consisting of only 0's and 1's in random order. Modify the array in-place to segregate 0s onto 
+the left side and 1s onto the right side of the array.
+
+Examples :
+
+Input: arr[] = [0, 0, 1, 1, 0]
+Output: [0, 0, 0, 1, 1]
+Explanation:  After segregation, all the 0's are on the left and 1's are on the right. Modified array will be 
+[0, 0, 0, 1, 1].
+Input: arr[] = [1, 1, 1, 1]
+Output: [1, 1, 1, 1]
+Explanation: There are no 0s in the given array, so the modified array is [1, 1, 1, 1]
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arr[i] ≤ 1 """
+
+#User function Template for python3
+
+class Solution:
+    def segregate0and1(self, arr):
+        # code here
+        res = arr.sort()
+        return res
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        arr = list(map(int, input().strip().split()))
+
+        obj = Solution()
+        obj.segregate0and1(arr)
+
+        print(*arr)
+
+# } Driver Code Ends
