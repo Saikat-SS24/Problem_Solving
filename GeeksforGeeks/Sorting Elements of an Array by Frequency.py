103f677b5c8b5fcc73d40c3f5f72fd5842252bbf
Saikat Sheet <saikatsheet48@gmail.com>
2024-08-28 11:22:47 +0530

    uploading solution

---
+++ GeeksforGeeks/Sorting Elements of an Array by Frequency.py
@@ -0,0 +1,78 @@
+""" Given an array of integers arr, sort the array according to the frequency of elements, i.e. elements that have 
+higher frequency comes first. If the frequencies of two elements are the same, then the smaller number comes first.
+
+Examples :
+
+Input: arr[] = [5, 5, 4, 6, 4]
+Output: [4, 4, 5, 5, 6]
+Explanation: The highest frequency here is 2. Both 5 and 4 have that frequency. Now since the frequencies are the 
+same the smaller element comes first. So 4 4 comes first then comes 5 5. Finally comes 6. The output is 4 4 5 5 6.
+
+Input: arr[] = [9, 9, 9, 2, 5]
+Output: [9, 9, 9, 2, 5]
+Explanation: The highest frequency here is 3. Element 9 has the highest frequency So 9 9 9 comes first. 
+Now both 2 and 5 have the same frequency. So we print smaller elements first. The output is 9 9 9 2 5.
+
+Expected Time Complexity: O(n*logn)
+Expected Space Complexity: O(n)
+
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i]≤ 106  """
+
+#User function Template for python3
+from collections import defaultdict
+
+class Solution:
+   
+    #Function to sort the array according to frequency of elements.
+    def sortByFreq(self,arr):
+        #code here
+        n = len(arr)
+        # arr -> Array to be sorted
+        # n   -> Length of Array
+    
+        # d is a hashmap(referred as dictionary in python)
+        d = defaultdict(lambda: 0)
+        for i in range(n):
+            d[arr[i]] += 1
+    
+        # Sorting the array 'arr' where key
+        # is the function based on which
+        # the array is sorted
+        # While sorting we want to give
+        # first priority to Frequency
+        # Then to value of item
+        arr.sort(key=lambda x: (-d[x], x)) 
+        
+        return (arr)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+
+        arr = list(map(int, input().strip().split()))
+        l = Solution().sortByFreq(arr)
+        print(*l)
+
+# } Driver Code Ends
