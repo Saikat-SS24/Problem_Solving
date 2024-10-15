79ddd3d9a2d0480587fae86d35076e066386186e
Saikat Sheet <saikatsheet48@gmail.com>
2024-10-15 11:04:17 +0530

    uploading solution

---
+++ GeeksforGeeks/Subarray range with given sum.py
@@ -0,0 +1,60 @@
+""" Given an unsorted array of integers arr[], and a target tar, determine the number of subarrays whose elements sum 
+up to the target value.
+
+Examples:
+
+Input: arr[] = [10, 2, -2, -20, 10] , tar = -10
+Output: 3
+Explanation: Subarrays with sum -10 are: [10, 2, -2, -20], [2, -2, -20, 10] and [-20, 10].
+
+Input: arr[] = [1, 4, 20, 3, 10, 5] , tar = 33
+Output: 1
+Explanation: Subarray with sum 33 is: [20,3,10].
+
+Expected Time Complexity: O(n)
+Expected Auxilary Space: O(n)
+
+Constraints:
+1 <= arr.size() <= 106
+-105 <= arr[i] <= 105
+-105 <= tar <= 105   """
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Complete this fuction
+    #Function to count the number of subarrays which adds to the given sum.
+    def subArraySum(self,arr, tar):
+        #Your code here
+        d={0:1}
+        count=0
+        cur_sum=0
+        for i in range(len(arr)):
+            cur_sum+=arr[i]
+            if cur_sum-tar in d:
+                count+=d[cur_sum-tar]
+            d[cur_sum]=d.get(cur_sum,0)+1
+        return count
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        tar= int(input())
+        ob = Solution()
+        res = ob.subArraySum(arr,tar)
+        print(res)
+        # print("~")
+        t -= 1
+
+# } Driver Code Ends
