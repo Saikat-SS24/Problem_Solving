827d424400070c973eb63c7eb74f7ff1f7317638
Saikat Sheet <saikatsheet48@gmail.com>
2024-10-18 10:56:21 +0530

    uploading solution

---
+++ GeeksforGeeks/Single Number.py
@@ -0,0 +1,52 @@
+""" Given an array arr[] of positive integers where every element appears even times except for one. Find that number 
+occurring an odd number of times.
+
+Examples:
+
+Input: arr[] = [1, 1, 2, 2, 2]
+Output: 2
+Explanation: In the given array all element appear two times except 2 which appears thrice.
+
+Input: arr[] = [8, 8, 7, 7, 6, 6, 1]
+Output: 1
+Explanation: In the given array all element appear two times except 1 which appears once.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ arr.size() ≤ 106
+0 ≤ arri ≤ 105  """
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    def getSingle(self,arr):
+        # code here
+        v = arr[0]
+        for i in arr[1:]:
+            v ^= i
+        return v
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        # k= int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.getSingle(arr)
+        print(res)
+        t -= 1
+
+
+# } Driver Code Ends
