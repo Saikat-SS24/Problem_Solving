0984247e4680a543bd8c3f6f57410adede156fbb
Saikat Sheet <saikatsheet48@gmail.com>
2024-12-29 19:33:53 +0530

    uploading solution

---
+++ GeeksforGeeks/Intersection_of_Two_arrays_with_Duplicate_Elements.py
@@ -0,0 +1,62 @@
+""" Given two integer arrays a[] and b[], you have to find the intersection of the two arrays. Intersection of two 
+arrays is said to be elements that are common in both arrays. The intersection should not have duplicate elements and 
+the result should contain items in any order.
+
+Note: The driver code will sort the resulting array in increasing order before printing.
+
+Examples:
+
+Input: a[] = [1, 2, 1, 3, 1], b[] = [3, 1, 3, 4, 1]
+Output: [1, 3]
+Explanation: 1 and 3 are the only common elements and we need to print only one occurrence of common elements.
+
+Input: a[] = [1, 1, 1], b[] = [1, 1, 1, 1, 1]
+Output: [1]
+Explanation: 1 is the only common element present in both the arrays.
+
+Input: a[] = [1, 2, 3], b[] = [4, 5, 6]
+Output: []
+Explanation: No common element in both the arrays.
+
+Constraints:
+1 ≤ a.size(), b.size() ≤ 105
+1 ≤ a[i], b[i] ≤ 105  """
+
+class Solution:
+    def intersectionWithDuplicates(self, a, b):
+        # code here
+        return list(set(a)&set(b))
+
+
+#{ 
+ # Driver Code Starts
+#Position this line where user code will be pasted.
+t = int(input().strip())
+while t > 0:
+    t -= 1
+    # Read first array
+    a = list(map(int, input().strip().split()))
+
+    # Read second array
+    b = list(map(int, input().strip().split()))
+
+    #input()  # to consume the empty line
+
+    # ADD Solution initialization
+    sln = Solution()
+
+    # Assuming numberofElementsInIntersection function is defined in Solution
+    res = sln.intersectionWithDuplicates(a, b)
+
+    # Sort the result
+    res.sort()
+
+    # Print the result
+    if not res:
+        print("[]")
+    else:
+        print(" ".join(map(str, res)))
+
+    print("~")
+
+# } Driver Code Ends
