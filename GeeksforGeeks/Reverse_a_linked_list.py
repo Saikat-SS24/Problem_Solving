8d9551acffe45346254c339e6aa71498330f2592
Saikat Sheet <saikatsheet48@gmail.com>
2025-01-18 11:11:58 +0530

    uploading solution

---
+++ GeeksforGeeks/Reverse_a_linked_list.py
@@ -0,0 +1,86 @@
+""" Given the head of a linked list, the task is to reverse this list and return the reversed head.
+
+Examples:
+
+Input: head: 1 -> 2 -> 3 -> 4 -> NULL
+Output: head: 4 -> 3 -> 2 -> 1 -> NULL
+Explanation:
+
+Input: head: 2 -> 7 -> 10 -> 9 -> 8 -> NULL
+Output: head: 8 -> 9 -> 10 -> 7 -> 2 -> NULL
+Explanation:
+
+Input: head: 10 -> NULL
+Output: 10 -> NULL
+Explanation:
+
+Constraints:
+1 <= number of nodes, data of nodes <= 105  """
+
+#function Template for python3
+
+"""
+# Node Class
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+"""
+
+class Solution:
+    def reverseList(self, head):
+        # Code here
+        prev, node = None, head
+        while node:
+            node.next, node, prev = prev, node.next, node
+        return prev
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class Node:
+
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+# Linked List Class
+class Linked_List:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+
+def printList(head):
+    tmp = head
+    while tmp:
+        print(tmp.data, end=' ')
+        tmp = tmp.next
+    print()
+
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+
+        arr = [int(x) for x in input().split()]
+
+        lis = Linked_List()
+        for i in arr:
+            lis.insert(i)
+
+        newHead = Solution().reverseList(lis.head)
+        printList(newHead)
+        print("~")
+
+# } Driver Code Ends
