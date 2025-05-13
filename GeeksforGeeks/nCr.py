90c15eaff7824c7720a8bbd1535ddbae4652e6d6
Saikat Sheet <saikatsheet48@gmail.com>
2025-05-13 11:01:27 +0530

    uploading solution

---
+++ GeeksforGeeks/nCr.py
@@ -0,0 +1,50 @@
+""" Given two integer values n and r, the task is to find the value of Binomial Coefficient nCr
+
+A binomial coefficient nCr can be defined as the coefficient of xr in the expansion of (1 + x)n that gives the number 
+of ways to choose r objects from a set of n objects without considering the order.
+The binomial coefficient nCr is calculated as : C(n,r) = n! / r! * (n-r) !
+Note: If r is greater than n, return 0.
+It is guaranteed that the value of nCr will fit within a 32-bit integer.
+
+Examples:
+
+Input: n = 5, r = 2
+Output: 10
+Explaination: The value of 5C2 is calculated as 5!/(5−2)!*2! = 5!/3!*2! = 10.
+
+Input: n = 2, r = 4
+Output: 0
+Explaination: Since r is greater than n, thus 2C4 = 0
+
+Input: n = 5, r = 0
+Output: 1
+Explaination: The value of 5C0 is calculated as 5!/(5−0)!*0! = 5!/5!*0! = 1.
+
+Constraints:
+1 ≤ n ≤ 100
+1 ≤ r ≤ 100 """
+
+#User function Template for python3
+
+class Solution:
+    def nCr(self, n, r):
+        # code here
+        res = 1
+        for i in range(1, r+1):
+            res = res * (n - r + i) // i
+            
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        r = int(input())
+        ob = Solution()
+        print(ob.nCr(n, r))
+        print("~")
+# } Driver Code Ends
